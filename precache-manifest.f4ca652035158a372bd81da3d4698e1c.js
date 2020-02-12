b7490d31134c6075dbeb70c2b9b21832fef37c3b
Senya <senyaking10@gmail.com>
2020-02-12 21:59:48 +0300

    Updates

---
+++ precache-manifest.f4ca652035158a372bd81da3d4698e1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba7148cc961449a036f89a35b072eb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "276920f8505ea86ed03e",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "5cb1799fa7835dc5a60e",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "276920f8505ea86ed03e",
+    "url": "/static/js/2.45fa9a84.chunk.js"
+  },
+  {
+    "revision": "5cb1799fa7835dc5a60e",
+    "url": "/static/js/main.2f9ce086.chunk.js"
+  },
+  {
+    "revision": "68f9fc2013e423920945",
+    "url": "/static/js/runtime-main.529ebd8a.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
